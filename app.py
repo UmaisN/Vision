a58e4eb762eb373f63345bd6758e4e3338ca705b
Umais Nisar <i190573@nu.edu.pk>
2023-06-12 18:13:36 +0500

    Firstest commit

--- app.py
+++ app.py
@@ -1,39 +1,57 @@
-from flask import Flask, render_template, request
 import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__, template_folder='template')
-
-# Set the upload folder and allowed extensions for file upload
-app.config['UPLOAD_FOLDER'] = 'uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'mp4', 'avi', 'mov', 'flv', 'wmv'}
-
-# Check if a filename is allowed based on the file extensions
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 9999 * 9999 * 9999
+	
 @app.route('/')
-def index():
-    return render_template('index.html')
+def upload_form():
+	return render_template('index.html')
 
-@app.route('/process-video', methods=['POST'])
-def process_video():
-    # Check if the POST request has a file part
-    if 'video_file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['video_file']
-    # If the user does not select a file, browser also
-    # submits an empty part without filename
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        # Save the file to the upload folder
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # Call your lane detection function here using the saved file path
-        # ...
-        return 'Video processing complete!'
-    else:
-        flash('Invalid file type')
-        return redirect(request.url)
+@app.route('/', methods=['POST'])
+def upload_video():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No video selected for uploading')
+		return redirect(request.url)
+	else:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		flash('Video successfully uploaded and displayed below')
+		return render_template('index.html', filename=filename)
+	
+def upload_image():
+	if 'file' not in request.files:
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename == '':
+		flash('No image selected for uploading')
+		return redirect(request.url)
+	if file:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		#print('upload_image filename: ' + filename)
+		flash('Image successfully uploaded and displayed below')
+		return render_template('index.html', filename=filename)
+	else:
+		flash('Allowed image types are -> png, jpg, jpeg, gif')
+		return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_video(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+def display_image(filename):
+	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+if __name__ == "__main__":
+    app.run(debug=False,host='0.0.0.0')
